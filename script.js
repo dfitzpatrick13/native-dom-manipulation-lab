6fa20b875c5e5f655fea61a3ad05175d45aa295f
“[dfitzpatrick13]” <[danfitz156@gmail.com]>
2023-11-07 16:16:40 -0500

    complete

--- script.js
+++ script.js
@@ -17,32 +17,57 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
-
+  const webTitle = document.getElementById("main-title")
+  webTitle.textContent = "Doms Websites"
   // Part 2
-
+  const bodyEl = document.querySelector("body")
+  bodyEl.style.backgroundColor = "orange"
 
   // Part 3
-
-
+  const favThings = document.getElementById("favorite-things")
+  const lastListItem = favThings.lastElementChild
+  favThings.removeChild(lastListItem)
   // Part 4
-
+  const specialTitleEl = document.querySelectorAll(".special-title")
+  specialTitleEl.forEach(function(element){
+    element.style.fontSize = "2rem";
+  });
 
   // Part 5
-
-
+  const pastRaces = document.getElementById("past-races")
+  const lastRaceItem = pastRaces.lastElementChild
+  pastRaces.removeChild(lastRaceItem)
   // Part 6
-
-
+  const newCity = document.createElement("li")
+  newCity.textContent = "Boston"
+  pastRaces.appendChild(newCity)
   // Part 7
+  const mainContent = document.querySelector(".main")
+  const newBlog = document.createElement("div")
+  newBlog.className = "blog-post-red"
 
+  const cityH1 = document.createElement("h1")
+  cityH1.textContent = ("Las Vegas")
+  
+  const cityDescript = document.createElement("p")
+  cityDescript.textContent = "Las vages be hot somtimess"
 
+  newBlog.appendChild(cityH1)
+  newBlog.appendChild(cityDescript)
   // Part 8
-
-
+  const quoteTitle = document.getElementById("quote-title")
+  quoteTitle.addEventListener("click", randomQuote)
   // Part 9
+  const blogPostEl = document.querySelectorAll(".blog-post")
 
+  blogPostEl.forEach(function (blogPost) {s
+    blogPost.addEventListener("mouseout", function () {
+      blogPost.classList.toggle("purple");
+    });
 
-
+    blogPost.addEventListener("mouseenter", function () {
+      blogPost.classList.toggle("red");
+    });
+  });
 
 });
